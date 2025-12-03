155e02876b5b38cbe4c973079a0bc0d0c42fd16d
high-masa <m-takahashi@juku-magic.com>
2025-12-03 18:56:10 +0900

    ffirebase

---
+++ firebaseapi.ts
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBp3hvhnBWSvq2TYvsTFmnOnqabnY7vx3g",
+    authDomain: "juku-booking.firebaseapp.com",
+    projectId: "juku-booking",
+    storageBucket: "juku-booking.firebasestorage.app",
+    messagingSenderId: "134258298426",
+    appId: "1:134258298426:web:24d901ef9fc92c9b0daf03"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
